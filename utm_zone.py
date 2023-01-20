f4d1ee8f79e3b25e9e982b6b638eb2080344c368
Pierre545 <57266944+Pierre545@users.noreply.github.com>
2023-01-20 14:23:50 +1100

    Add files via upload

---
+++ utm_zone.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 15:16:48 2023
+
+@author: pierreaudisio
+"""
+
+import utm
+
+utm_east, utm_north, utm_zone, utm_letter = utm.from_latlon(lat, lon)
